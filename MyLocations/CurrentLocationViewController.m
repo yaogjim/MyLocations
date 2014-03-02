0f9a11a47bb320986cf9056da17ceec104f9cb1c
Derek Bassett <pabassett@gmail.com>
2014-03-01 19:13:05 -0700

    Handling errors

--- MyLocations/CurrentLocationViewController.m
+++ MyLocations/CurrentLocationViewController.m
@@ -15,6 +15,9 @@
 @implementation CurrentLocationViewController
 {
   CLLocationManager *_locationManager;
+  CLLocation *_location;
+  BOOL _updatingLocation;
+  NSError *_lastLocationError;
 }
 
 - (id)initWithCoder:(NSCoder *)aDecoder
@@ -27,8 +30,8 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+  [super viewDidLoad];
+  [self updateLabels];
 }
 
 - (void)didReceiveMemoryWarning
@@ -49,6 +52,15 @@
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
 {
   NSLog(@"didFailWithError %@", error);
+
+  if (error.code == kCLErrorLocationUnknown) {
+    return;
+  }
+
+  [self stopLocationManager];
+  _lastLocationError = error;
+
+  [self updateLabels];
 }
 
 - (void) locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
@@ -56,6 +68,48 @@
   CLLocation *newLocation = [locations lastObject];
 
   NSLog(@"didUpdateLocations %@", newLocation);
+  _location = newLocation;
+  [self updateLabels];
+}
+
+- (void) updateLabels
+{
+  if (_location != nil) {
+    self.latitudeLabel.text = [NSString stringWithFormat: @"%.8f", _location.coordinate.latitude];
+    self.longitudeLabel.text = [NSString stringWithFormat: @"%.8f", _location.coordinate.longitude];
+    self.tagButton.hidden = NO;
+    self.messageLabel.text = @"";
+  } else {
+    self.latitudeLabel.text = @"";
+    self.longitudeLabel.text = @"";
+    self.addressLabel.text = @"";
+    self.tagButton.hidden = YES;
+
+    NSString *statusMessage;
+    if (_lastLocationError != nil) {
+      if ([_lastLocationError.domain isEqualToString:kCLErrorDomain] && _lastLocationError.code == kCLErrorDenied) {
+        statusMessage = @"Location Services Disabled";
+      } else {
+        statusMessage = @"Error Getting Location";
+      }
+    } else if (![CLLocationManager locationServicesEnabled]) {
+      statusMessage = @"Location Services Disabled";
+    } else if (_updatingLocation){
+      statusMessage = @"Searching...";
+    } else {
+      statusMessage = @"Press the Button to Start";
+    }
+    self.messageLabel.text = statusMessage;
+  }
+}
+
+- (void)stopLocationManager
+{
+  if (_updatingLocation){
+    [_locationManager stopUpdatingLocation];
+    _locationManager.delegate = nil;
+    _updatingLocation = NO;
+  }
 }
 
 @end
