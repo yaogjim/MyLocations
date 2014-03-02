ca3882257445d9d2f3ebb978c7cf604f8ac9b748
Derek Bassett <pabassett@gmail.com>
2014-03-02 12:55:23 -0700

    Making my own cell

---
+++ MyLocations/LocationDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationDetailsViewController.h
+//  MyLocations
+//
+//  Created by Derek Bassett on 3/1/14.
+//  Copyright (c) 2014 Two Cavemen LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationDetailsViewController : UITableViewController
+
+@end
