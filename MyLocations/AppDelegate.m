0a7cc90b09ee514861b6e52e57d0764cf6b57a49
Derek Bassett <pabassett@gmail.com>
2014-03-09 22:03:06 -0600

    Handling Core Data errors

--- MyLocations/AppDelegate.m
+++ MyLocations/AppDelegate.m
@@ -9,7 +9,9 @@
 #import "AppDelegate.h"
 #import "CurrentLocationViewController.h"
 
-@interface AppDelegate ()
+NSString * const ManagedObjectContextSaveDidFailNotification = @"ManagedObjectContextSaveDidFailNotification";
+
+@interface AppDelegate () <UIAlertViewDelegate>
 
 @property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
 @property (nonatomic, strong) NSManagedObjectModel *managedObjectModel;
@@ -26,6 +28,8 @@
   CurrentLocationViewController *currentLocationViewController = (CurrentLocationViewController *)tabBarController.viewControllers[0];
 
   currentLocationViewController.managedObjectContext = self.managedObjectContext;
+
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(fatalCoreDataError:) name:ManagedObjectContextSaveDidFailNotification object:nil];
   return YES;
 }
 							
@@ -104,5 +108,20 @@
     }
   }
   return _managedObjectContext;
+}
+
+- (void) fatalCoreDataError:(NSNotification *)notification
+{
+  UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Internal Error", nil)
+                                                      message:NSLocalizedString(@"There was a fatal error in the app and it cannot continue.\n\nPress OK to terminate the app.  Sorry for the inconvenience.", nil)
+                                                     delegate:self cancelButtonTitle:NSLocalizedString(@"OK", nil) otherButtonTitles:nil];
+  [alertView show];
+}
+
+#pragma mark - UIAlertViewDelegate
+
+-(void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+  abort();
 }
 @end
